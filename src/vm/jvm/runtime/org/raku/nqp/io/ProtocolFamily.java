cc0744b29d3ad1c2308ce1b61ef13f3156c3bc73
Ben Davies <kaiepi@outlook.com>
2021-03-08 09:25:27 -0400

    [jvm] Add a ProtocolFamily class
    This contains each of the hardcoded protocol families referred to by
    runtime ops.

---
+++ src/vm/jvm/runtime/org/raku/nqp/io/ProtocolFamily.java
@@ -0,0 +1,9 @@
+package org.raku.nqp.io;
+
+/* Stores hardcoded families of network addresses. */
+public final class ProtocolFamily {
+    public static final int UNSPEC = 0;
+    public static final int INET   = 1;
+    public static final int INET6  = 2;
+    public static final int UNIX   = 3;
+}
