07ece7049601176c30e68ec507a37bcab0639a49
Ben Davies <kaiepi@outlook.com>
2021-03-18 15:13:17 -0300

    [jvm] Stop emitting host/port pairs from sockets
    This is more than just replacing host/port pairs with addresses:
    
    - There's no need to be emitting these when connecting, listening, or
      accepting sockets anymore.
    - Some ops' callback parameter orderings have changed in the name of
      optimization.
    - A portion of the backend's networking code has been final-ized.

--- src/vm/jvm/runtime/org/raku/nqp/io/AsyncServerSocketHandle.java
+++ src/vm/jvm/runtime/org/raku/nqp/io/AsyncServerSocketHandle.java
@@ -20,162 +20,84 @@ import org.raku.nqp.sixmodel.reprs.IOHandleInstance;
 
 public class AsyncServerSocketHandle implements IIOBindable, IIOCancelable, IIOAddressable {
 
-    AsynchronousServerSocketChannel listenChan;
+    final AsynchronousServerSocketChannel listenChan;
 
-    public AsyncServerSocketHandle(ThreadContext tc) {
+    public AsyncServerSocketHandle(final ThreadContext tc) {
         try {
             listenChan = AsynchronousServerSocketChannel.open();
-        } catch (IOException e) {
-            ExceptionHandling.dieInternal(tc, e);
+        } catch (final IOException e) {
+            throw ExceptionHandling.dieInternal(tc, e);
         }
     }
 
     @Override
-    public void bind(ThreadContext tc, String host, int port, int backlog) {
+    public void bind(final ThreadContext tc, final String host, final int port, final int backlog) {
         try {
             InetSocketAddress addr = new InetSocketAddress(host, port);
             listenChan.bind(addr, backlog);
-        } catch (UnresolvedAddressException uae) {
+        } catch (final UnresolvedAddressException uae) {
             ExceptionHandling.dieInternal(tc, "Failed to resolve host name");
-        } catch (IOException e) {
+        } catch (final IOException e) {
             throw ExceptionHandling.dieInternal(tc, e);
         }
     }
 
     public void accept(final ThreadContext tc, final AsyncTaskInstance task) {
+        final HLLConfig      hllConfig = tc.curFrame.codeRef.staticInfo.compUnit.hllConfig;
+        final SixModelObject IOType    = hllConfig.ioType;
+        final SixModelObject Array     = hllConfig.listType;
+        final SixModelObject Int       = hllConfig.intBoxType;
+        final SixModelObject Str       = hllConfig.strBoxType;
 
         final CompletionHandler<AsynchronousSocketChannel, AsyncTaskInstance> handler
             = new CompletionHandler<AsynchronousSocketChannel, AsyncTaskInstance>() {
 
-            HLLConfig hllConfig = tc.curFrame.codeRef.staticInfo.compUnit.hllConfig;
-            final SixModelObject IOType = hllConfig.ioType;
-            final SixModelObject Array = hllConfig.listType;
-            final SixModelObject Null = hllConfig.nullValue;
-            final SixModelObject Int = hllConfig.intBoxType;
-            final SixModelObject Str = hllConfig.strBoxType;
-
             @Override
-            public void completed(AsynchronousSocketChannel channel, AsyncTaskInstance task) {
+            public void completed(final AsynchronousSocketChannel channel, final AsyncTaskInstance task) {
                 listenChan.accept(task, this);
 
-                InetSocketAddress localAddress;
-                InetSocketAddress remoteAddress;
-
-                try {
-                    localAddress = (InetSocketAddress) channel.getLocalAddress();
-                } catch (IOException e) {
-                    throw ExceptionHandling.dieInternal(tc, e);
-                }
-                String socketHost = localAddress.getAddress().getHostAddress();
-                if (socketHost.equals("0:0:0:0:0:0:0:1"))
-                    socketHost = "::1";
-                int socketPort = localAddress.getPort();
-
-                try {
-                    remoteAddress = (InetSocketAddress) channel.getRemoteAddress();
-                } catch (IOException e) {
-                    throw ExceptionHandling.dieInternal(tc, e);
-                }
-                String peerHost = remoteAddress.getAddress().getHostAddress();
-                if (peerHost.equals("0:0:0:0:0:0:0:1"))
-                    peerHost = "::1";
-                int peerPort = remoteAddress.getPort();
-
-                ThreadContext curTC = tc.gc.getCurrentThreadContext();
-
-                AsyncSocketHandle clientHandle = new AsyncSocketHandle(curTC, channel);
-                IOHandleInstance clientIoHandle = (IOHandleInstance) IOType.st.REPR.allocate(curTC,
-                        IOType.st);
-                clientIoHandle.handle = clientHandle;
-
-                IOHandleInstance serverIoHandle = (IOHandleInstance) IOType.st.REPR.allocate(curTC,
-                        IOType.st);
-                serverIoHandle.handle = this;
-
-                SixModelObject result = Array.st.REPR.allocate(curTC, Array.st);
+                final ThreadContext    curTC          = tc.gc.getCurrentThreadContext();
+                final SixModelObject   result         = Array.st.REPR.allocate(curTC, Array.st);
+                final IOHandleInstance clientIoHandle = (IOHandleInstance) IOType.st.REPR.allocate(curTC, IOType.st);
+                clientIoHandle.handle = new AsyncSocketHandle(curTC, channel);
                 result.push_boxed(curTC, task.schedulee);
                 result.push_boxed(curTC, clientIoHandle);
-                result.push_boxed(curTC, Null);
-                result.push_boxed(curTC, Ops.box_s(peerHost, Str, curTC));
-                result.push_boxed(curTC, Ops.box_i(peerPort, Int, curTC));
-                result.push_boxed(curTC, serverIoHandle);
-                result.push_boxed(curTC, Ops.box_s(socketHost, Str, curTC));
-                result.push_boxed(curTC, Ops.box_i(socketPort, Int, curTC));
-
+                result.push_boxed(curTC, Str);
                 ((ConcBlockingQueueInstance) task.queue).push_boxed(curTC, result);
             }
 
             @Override
             public void failed(Throwable exc, AsyncTaskInstance task) {
-                ThreadContext curTC = tc.gc.getCurrentThreadContext();
-                SixModelObject result = Array.st.REPR.allocate(curTC, Array.st);
+                final ThreadContext  curTC  = tc.gc.getCurrentThreadContext();
+                final SixModelObject result = Array.st.REPR.allocate(curTC, Array.st);
                 result.push_boxed(curTC, task.schedulee);
                 result.push_boxed(curTC, IOType);
-                result.push_boxed(curTC, Ops.box_s(exc.toString(), Str, curTC));
-                result.push_boxed(curTC, Str);
-                result.push_boxed(curTC, Int);
                 result.push_boxed(curTC, IOType);
-                result.push_boxed(curTC, Str);
-                result.push_boxed(curTC, Int);
+                result.push_boxed(curTC, Ops.box_s(exc.toString(), Str, curTC));
+                ((ConcBlockingQueueInstance) task.queue).push_boxed(curTC, result);
             }
         };
 
-        try {
-            InetSocketAddress localAddress;
-
-            HLLConfig hllConfig = tc.curFrame.codeRef.staticInfo.compUnit.hllConfig;
-            final SixModelObject IOType = hllConfig.ioType;
-            final SixModelObject Array = hllConfig.listType;
-            final SixModelObject Null = hllConfig.nullValue;
-            final SixModelObject Int = hllConfig.intBoxType;
-            final SixModelObject Str = hllConfig.strBoxType;
-
-            try {
-                localAddress = (InetSocketAddress) listenChan.getLocalAddress();
-            } catch (IOException e) {
-                throw ExceptionHandling.dieInternal(tc, e);
-            }
-
-            String socketHost = localAddress.getAddress().getHostAddress();
-            if (socketHost.equals("0:0:0:0:0:0:0:1"))
-                socketHost = "::1";
-            int socketPort = localAddress.getPort();
-
-            ThreadContext curTC = tc.gc.getCurrentThreadContext();
-
-            IOHandleInstance ioHandle = (IOHandleInstance) IOType.st.REPR.allocate(curTC,
-                    IOType.st);
-            ioHandle.handle = this;
-
-            SixModelObject result = Array.st.REPR.allocate(curTC, Array.st);
-
-            result.push_boxed(curTC, task.schedulee);
-            result.push_boxed(curTC, IOType);
-            result.push_boxed(curTC, Null);
-            result.push_boxed(curTC, Str);
-            result.push_boxed(curTC, Int);
-            result.push_boxed(curTC, ioHandle);
-            result.push_boxed(curTC, Ops.box_s(socketHost, Str, curTC));
-            result.push_boxed(curTC, Ops.box_i(socketPort, Int, curTC));
-
-            ((ConcBlockingQueueInstance) task.queue).push_boxed(curTC, result);
-        }
-        catch (Exception e) {
-            throw ExceptionHandling.dieInternal(tc, e);
-        }
-
+        final ThreadContext    curTC          = tc.gc.getCurrentThreadContext();
+        final SixModelObject   result         = Array.st.REPR.allocate(curTC, Array.st);
+        final IOHandleInstance serverIoHandle = (IOHandleInstance) IOType.st.REPR.allocate(curTC, IOType.st);
+        serverIoHandle.handle = this;
+        result.push_boxed(curTC, task.schedulee);
+        result.push_boxed(curTC, IOType);
+        result.push_boxed(curTC, serverIoHandle);
+        ((ConcBlockingQueueInstance) task.queue).push_boxed(curTC, result);
         try {
             listenChan.accept(task, handler);
-        } catch (NotYetBoundException e) {
+        } catch (final NotYetBoundException e) {
             throw ExceptionHandling.dieInternal(tc, e);
         }
     }
 
     @Override
-    public void cancel(ThreadContext tc) {
+    public void cancel(final ThreadContext tc) {
         try {
             listenChan.close();
-        } catch (IOException e) {
+        } catch (final IOException e) {
             throw ExceptionHandling.dieInternal(tc, e);
         }
     }
--- src/vm/jvm/runtime/org/raku/nqp/io/AsyncSocketHandle.java
+++ src/vm/jvm/runtime/org/raku/nqp/io/AsyncSocketHandle.java
@@ -37,59 +37,43 @@ public class AsyncSocketHandle implements IIOClosable, IIOCancelable, IIOAddress
 
     public void connect(final ThreadContext tc, String host, int port,
             final AsyncTaskInstance task) {
+        final HLLConfig      hllConfig = tc.curFrame.codeRef.staticInfo.compUnit.hllConfig;
+        final SixModelObject IOType    = hllConfig.ioType;
+        final SixModelObject Array     = hllConfig.listType;
+        final SixModelObject Int       = hllConfig.intBoxType;
+        final SixModelObject Str       = hllConfig.strBoxType;
 
         final CompletionHandler<Void, AsyncTaskInstance> handler
             = new CompletionHandler<Void, AsyncTaskInstance>() {
 
-            HLLConfig hllConfig = tc.curFrame.codeRef.staticInfo.compUnit.hllConfig;
-            final SixModelObject IOType = hllConfig.ioType;
-            final SixModelObject Array = hllConfig.listType;
-            final SixModelObject Int = hllConfig.intBoxType;
-            final SixModelObject Str = hllConfig.strBoxType;
-
             @Override
-            public void completed(Void v, AsyncTaskInstance task) {
-                ThreadContext curTC = tc.gc.getCurrentThreadContext();
-
-                IOHandleInstance ioHandle = (IOHandleInstance) IOType.st.REPR.allocate(curTC,
-                        IOType.st);
+            public void completed(final Void v, final AsyncTaskInstance task) {
+                final ThreadContext    curTC    = tc.gc.getCurrentThreadContext();
+                final SixModelObject   result   = Array.st.REPR.allocate(curTC, Array.st);
+                final IOHandleInstance ioHandle = (IOHandleInstance) IOType.st.REPR.allocate(curTC, IOType.st);
                 ioHandle.handle = task.handle;
-                callback(curTC, task, ioHandle, Str,
-                    Ops.box_s(host, Str, curTC),
-                    Ops.box_i(port, Int, curTC),
-                    Ops.box_s(host, Str, curTC),  // TODO send socketHost
-                    Ops.box_i(port, Int, curTC)   // TODO send socketPort
-                );
+                result.push_boxed(curTC, task.schedulee);
+                result.push_boxed(curTC, ioHandle);
+                result.push_boxed(curTC, Str);
+                ((ConcBlockingQueueInstance) task.queue).push_boxed(curTC, result);
             }
 
             @Override
-            public void failed(Throwable t, AsyncTaskInstance task) {
-                ThreadContext curTC = tc.gc.getCurrentThreadContext();
-                callback(curTC, task, IOType,
-                    Ops.box_s(t.toString(), Str, curTC), Str, Int, Str, Int);
-            }
-
-            protected void callback(ThreadContext tc, AsyncTaskInstance task,
-                    SixModelObject ioHandle, SixModelObject err,
-                    SixModelObject peerHost, SixModelObject peerPort,
-                    SixModelObject socketHost, SixModelObject socketPort) {
-                SixModelObject result = Array.st.REPR.allocate(tc, Array.st);
-                result.push_boxed(tc, task.schedulee);
-                result.push_boxed(tc, ioHandle);
-                result.push_boxed(tc, err);
-                result.push_boxed(tc, peerHost);
-                result.push_boxed(tc, peerPort);
-                result.push_boxed(tc, socketHost);
-                result.push_boxed(tc, socketPort);
-                ((ConcBlockingQueueInstance) task.queue).push_boxed(tc, result);
+            public void failed(final Throwable t, final AsyncTaskInstance task) {
+                final ThreadContext  curTC  = tc.gc.getCurrentThreadContext();
+                final SixModelObject result = Array.st.REPR.allocate(curTC, Array.st);
+                result.push_boxed(curTC, task.schedulee);
+                result.push_boxed(curTC, IOType);
+                result.push_boxed(curTC, Ops.box_s(t.toString(), Str, curTC));
+                ((ConcBlockingQueueInstance) task.queue).push_boxed(curTC, result);
             }
         };
 
         try {
             InetSocketAddress addr = new InetSocketAddress(host, port);
             channel.connect(addr, task, handler);
-        } catch (Throwable e) {
-            throw ExceptionHandling.dieInternal(tc, e);
+        } catch (final Throwable t) {
+            throw ExceptionHandling.dieInternal(tc, t);
         }
     }
 
@@ -99,40 +83,39 @@ public class AsyncSocketHandle implements IIOClosable, IIOCancelable, IIOAddress
     }
 
     private void writeByteBuffer(final ThreadContext tc, final AsyncTaskInstance task, ByteBuffer buffer) {
+        final HLLConfig      hllConfig = tc.curFrame.codeRef.staticInfo.compUnit.hllConfig;
+        final SixModelObject Array     = hllConfig.listType;
+        final SixModelObject Int       = hllConfig.intBoxType;
+        final SixModelObject Str       = hllConfig.strBoxType;
+
+        final CompletionHandler<Integer, AsyncTaskInstance> handler
+            = new CompletionHandler<Integer, AsyncTaskInstance>() {
+
+            @Override
+            public void completed(Integer bytesWritten, AsyncTaskInstance task) {
+                final ThreadContext  curTC  = tc.gc.getCurrentThreadContext();
+                final SixModelObject result = Array.st.REPR.allocate(curTC, Array.st);
+                result.push_boxed(curTC, task.schedulee);
+                result.push_boxed(curTC, Ops.box_i(bytesWritten, Int, curTC));
+                result.push_boxed(curTC, Str);
+                ((ConcBlockingQueueInstance) task.queue).push_boxed(curTC, result);
+            }
+
+            @Override
+            public void failed(Throwable t, AsyncTaskInstance attachment) {
+                final ThreadContext  curTC  = tc.gc.getCurrentThreadContext();
+                final SixModelObject result = Array.st.REPR.allocate(curTC, Array.st);
+                result.push_boxed(curTC, task.schedulee);
+                result.push_boxed(curTC, Int);
+                result.push_boxed(curTC, Ops.box_s(t.toString(), Str, curTC));
+                ((ConcBlockingQueueInstance) task.queue).push_boxed(curTC, result);
+            }
+        };
+
         try {
-            HLLConfig hllConfig = tc.curFrame.codeRef.staticInfo.compUnit.hllConfig;
-            final SixModelObject Array = hllConfig.listType;
-            final SixModelObject Int = hllConfig.intBoxType;
-            final SixModelObject Null = hllConfig.nullValue;
-            final SixModelObject Str = hllConfig.strBoxType;
-
-            CompletionHandler<Integer, AsyncTaskInstance> handler
-                = new CompletionHandler<Integer, AsyncTaskInstance>() {
-
-                @Override
-                public void completed(Integer bytesWritten, AsyncTaskInstance task) {
-                    ThreadContext curTC = tc.gc.getCurrentThreadContext();
-                    callback(curTC, task, Ops.box_i(bytesWritten, Int, curTC), Null);
-                }
-
-                @Override
-                public void failed(Throwable t, AsyncTaskInstance attachment) {
-                    ThreadContext curTC = tc.gc.getCurrentThreadContext();
-                    callback(curTC, task, Str, Ops.box_s(t.toString(), Str, curTC));
-                }
-
-                protected void callback(ThreadContext tc, AsyncTaskInstance task, SixModelObject bytesWritten, SixModelObject err) {
-                    SixModelObject result = Array.st.REPR.allocate(tc, Array.st);
-                    result.push_boxed(tc, task.schedulee);
-                    result.push_boxed(tc, bytesWritten);
-                    result.push_boxed(tc, err);
-                    ((ConcBlockingQueueInstance) task.queue).push_boxed(tc, result);
-                }
-            };
-
             channel.write(buffer, task, handler);
-        } catch (Throwable e) {
-            throw ExceptionHandling.dieInternal(tc, e);
+        } catch (final Throwable t) {
+            throw ExceptionHandling.dieInternal(tc, t);
         }
     }
 
@@ -154,75 +137,87 @@ public class AsyncSocketHandle implements IIOClosable, IIOCancelable, IIOAddress
     }
 
     private void readSocket(final ThreadContext tc, final AsyncTaskInstance task, final Decoder decoder) {
-        final ByteBuffer readBuffer = ByteBuffer.allocate(32768);
+        final ByteBuffer     readBuffer  = ByteBuffer.allocate(32768);
+        final SixModelObject BOOTAddress = tc.gc.BOOTAddress;
+        final HLLConfig      hllConfig   = tc.curFrame.codeRef.staticInfo.compUnit.hllConfig;
+        final SixModelObject Array       = hllConfig.listType;
+        final SixModelObject Int         = hllConfig.intBoxType;
+        final SixModelObject Str         = hllConfig.strBoxType;
 
-        HLLConfig hllConfig = tc.curFrame.codeRef.staticInfo.compUnit.hllConfig;
-        final SixModelObject Array = hllConfig.listType;
-        final SixModelObject Int = hllConfig.intBoxType;
-        final SixModelObject Str = hllConfig.strBoxType;
-        final SixModelObject Null = hllConfig.nullValue;
-
-        CompletionHandler<Integer, AsyncTaskInstance> handler
-        = new CompletionHandler<Integer, AsyncTaskInstance>() {
+        final CompletionHandler<Integer, AsyncTaskInstance> handler
+            = new CompletionHandler<Integer, AsyncTaskInstance>() {
 
             @Override
-            public void completed(Integer numRead, AsyncTaskInstance task) {
-                ThreadContext curTC = tc.gc.getCurrentThreadContext();
-
+            public void completed(final Integer numRead, final AsyncTaskInstance task) {
                 try {
+                    final ThreadContext curTC = tc.gc.getCurrentThreadContext();
                     if (numRead == -1) {
-                        callback(curTC, task, task.seq, Str, Null);
-                    } else {
+                        final SixModelObject result = Array.st.REPR.allocate(curTC, Array.st);
+                        result.push_boxed(curTC, task.schedulee);
+                        result.push_boxed(curTC, Ops.box_i(task.seq, Int, curTC));
+                        result.push_boxed(curTC, Str);
+                        result.push_boxed(curTC, Int);
+                        result.push_boxed(curTC, BOOTAddress);
+                        result.push_boxed(curTC, Str);
+                        ((ConcBlockingQueueInstance) task.queue).push_boxed(curTC, result);
+                    }
+                    else {
+                        final SixModelObject decoded;
                         readBuffer.flip();
-                        SixModelObject decoded = decoder.decode(tc, readBuffer, numRead);
+                        decoded = decoder.decode(tc, readBuffer, numRead);
                         readBuffer.compact();
 
-                        callback(curTC, task, task.seq++, decoded, Null);
+                        final SixModelObject result = Array.st.REPR.allocate(curTC, Array.st);
+                        result.push_boxed(curTC, task.schedulee);
+                        result.push_boxed(curTC, Ops.box_i(task.seq++, Int, curTC));
+                        result.push_boxed(curTC, decoded);
+                        result.push_boxed(curTC, Int);
+                        result.push_boxed(curTC, BOOTAddress);
+                        result.push_boxed(curTC, Str);
+                        ((ConcBlockingQueueInstance) task.queue).push_boxed(curTC, result);
 
                         channel.read(readBuffer, task, this);
                     }
-                } catch (Throwable t) {
+                } catch (final Throwable t) {
                     failed(t, task);
                 }
             }
 
             @Override
-            public void failed(Throwable t, AsyncTaskInstance task) {
-                ThreadContext curTC = tc.gc.getCurrentThreadContext();
-                SixModelObject err = (t instanceof AsynchronousCloseException
-                    || t instanceof ClosedChannelException)
-                        ? Str : Ops.box_s(t.toString(), Str, curTC);
-                callback(curTC, task, -1, Str, err);
-            }
+            public void failed(final Throwable t, final AsyncTaskInstance task) {
+                if (t instanceof AsynchronousCloseException || t instanceof ClosedChannelException)
+                    return;
 
-            protected void callback(ThreadContext tc, AsyncTaskInstance task, long seq, SixModelObject str, SixModelObject err) {
-                SixModelObject result = Array.st.REPR.allocate(tc, Array.st);
-                result.push_boxed(tc, task.schedulee);
-                result.push_boxed(tc, Ops.box_i(seq, Int, tc));
-                result.push_boxed(tc, str);
-                result.push_boxed(tc, err);
-                ((ConcBlockingQueueInstance) task.queue).push_boxed(tc, result);
+                final ThreadContext  curTC  = tc.gc.getCurrentThreadContext();
+                final SixModelObject result = Array.st.REPR.allocate(curTC, Array.st);
+                result.push_boxed(curTC, task.schedulee);
+                result.push_boxed(curTC, Int);
+                result.push_boxed(curTC, Str);
+                result.push_boxed(curTC, Int);
+                result.push_boxed(curTC, BOOTAddress);
+                result.push_boxed(curTC, Ops.box_s(t.toString(), Str, curTC));
+                ((ConcBlockingQueueInstance) task.queue).push_boxed(curTC, result);
             }
         };
 
         try {
             channel.read(readBuffer, task, handler);
-        } catch (Throwable t) {
+        } catch (final Throwable t) {
             handler.failed(t, task);
         }
     }
 
     @Override
-    public void close(ThreadContext tc) {
+    public void close(final ThreadContext tc) {
         try {
             channel.close();
-        } catch (IOException e) {
+        } catch (final IOException e) {
             throw ExceptionHandling.dieInternal(tc, e);
         }
     }
 
     @Override
-    public void cancel(ThreadContext tc) {
+    public void cancel(final ThreadContext tc) {
         close(tc);
     }
 
